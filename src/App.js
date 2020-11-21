046941163635b4ef67960ef9b86affa25d5f703d
zac-evans <zacevanscoding@gmail.com>
2020-11-21 12:48:31 -0500

    added _redirects file

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ export default class App extends Component {
             <Switch>
               <Route path="/register" component={Register} />
               <Route path="/login" component={Login} />
+              <Route exact path="/" component={Carouselapp} />
             </Switch>
             <Redirect push to="/login" />
           </Router>
